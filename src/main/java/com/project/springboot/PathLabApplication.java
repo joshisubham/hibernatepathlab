70383cb08ad35e0306edf501144ae149debaa6ab
Subham <subhamjoshi466@gmail.com>
2023-06-08 02:17:39 +0530

    New 2nd Commit

--- src/main/java/com/project/springboot/PathLabApplication.java
+++ src/main/java/com/project/springboot/PathLabApplication.java
@@ -9,6 +9,11 @@ import org.springframework.web.client.RestTemplate;
 //Check this Line
 
 
+
+
+
+
+
 @SpringBootApplication
 //@EntityScan("com.project.springboot.bean")
 public class PathLabApplication {
